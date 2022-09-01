424c2674c0caa98c957f608fd0dec5606c8d360e
Enrico <enricobaroni@hotmail.es>
2022-09-01 18:46:42 +0200

    Agregar archivos de proyecto.

---
+++ Carta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baraja
+{
+    class Carta
+    {
+        private int numero;
+        private string[] palos = {"Diamantes", "Picas", "Corazones", "Treboles" };
+        private int palo;
+
+        //Constructor
+        public Carta(int numeroCarta, int paloCarta)
+        {
+            numero = numeroCarta;
+            palo = paloCarta;
+        }
+        public override string ToString()
+        {
+            return (numero + " de " + palos[palo]);
+        }
+        
+        //GETTERS Y SETTERS
+        public int Numero
+        {
+            get { return numero; }
+            set { numero = value; }
+        }
+        public int Palo
+        {
+            get { return palo; }
+            set { palo = value; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baraja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Baraja baraja = new Baraja();
+            //baraja.NumeroCartas();
+            //baraja.RobaCarta();
+            //baraja.CogeCarta(2);
+            //baraja.CogeCartaAlAzar();
+            //baraja.Barajar();
+            //baraja.ToString();
+            Console.ReadKey();
+        }
+    }
+}
